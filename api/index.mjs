ea6a8d1a8fc356ed41ddd31125c493fe91bb7439
Reply AI <replyai@ctomail.io>
2026-07-02 04:49:15 +0000

    Clean api directory

--- api/index.mjs
+++
@@ -1,7 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const server = require('./ssr.mjs');
-export default async function handler(req) {
-  const response = await server.fetch(req);
-  return response;
-}
